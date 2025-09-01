038799b40fcd5485a16b289def5ba73ec1892bd8
Abderahmane Nouar <abderahmane.nouar@ext.adeo.com>
2025-09-01 22:31:56 +0200

    feat(): server @ $fetch to get data

--- app/pages/product/catalog/index.vue
+++ app/pages/product/catalog/index.vue
@@ -2,13 +2,31 @@
   definePageMeta({
     layout: 'catalog'
   }); // on peut en faire aussi un layout spécifique dans un composant en question ex: <NuxtLayout name="catalog">
-
+const products = await $fetch('/api/products'); // faire attention pour l'usage , appel deux fois (cote client & serveur), c'est bien de l'utiliser sur un event click (client side)
 </script>
 
 <template>
   <div>
-    Page: Product Catalog
-  </div>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        {{ product.name }} - {{ product.price }}€
+      </li>
+    </ul>
+ </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  padding: 12px 16px;
+  border-bottom: 1px solid #eee;
+}
+
+li:hover {
+  background-color: #f5f5f5;
+}
+</style>
---
+++ server/api/products.ts
@@ -0,0 +1,12 @@
+export default eventHandler(async () => {
+  const products = await getProducts();
+  return products;
+});
+
+function getProducts() {
+    return [
+        { id: 1, name: 'Product 1', price: 100 },
+        { id: 2, name: 'Product 2', price: 200 },
+        { id: 3, name: 'Product 3', price: 300 }
+    ]
+}
