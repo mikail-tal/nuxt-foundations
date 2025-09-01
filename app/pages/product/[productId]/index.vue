dd313f667ef378e2a31f97cf5498c007e188086b
Abderahmane Nouar <abderahmane.nouar@ext.adeo.com>
2025-09-01 22:10:27 +0200

    chore(): init project & nuxt page , dynamic routes

---
+++ app/pages/product/[productId]/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<template>
+  <div>
+    Page: product Page with ID: {{ route.params.productId }}
+  </div>
+</template>
+
+<style scoped></style>
